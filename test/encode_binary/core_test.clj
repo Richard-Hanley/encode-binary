b2b1ff63e5ca43e477e2540be63f171d72ec31c0
Richard Hanley <richard@rhanley.com>
2019-04-19 16:56:54 -0500

    Adding tests for encoding and decoding

--- test/encode_binary/core_test.clj
+++ test/encode_binary/core_test.clj
@@ -16,35 +16,35 @@
 (s/def :little/float (e/floating-primitive Float))
 (s/def :little/double (e/floating-primitive Double))
 
-(s/def :big/int8 (e/primitive Byte ::e/order :big))
-(s/def :big/int16 (e/primitive Short ::e/order :big))
-(s/def :big/int32 (e/primitive Integer ::e/order :big))
-(s/def :big/int64 (e/primitive Long ::e/order :big))
-(s/def :big/uint8 (e/unsigned-primitive Byte ::e/order :big))
-(s/def :big/uint16 (e/unsigned-primitive Short ::e/order :big))
-(s/def :big/uint32 (e/unsigned-primitive Integer ::e/order :big))
-(s/def :big/float (e/floating-primitive Float ::e/order :big))
-(s/def :big/double (e/floating-primitive Double ::e/order :big))
+(s/def :big/int8 (e/primitive Byte :order :big))
+(s/def :big/int16 (e/primitive Short :order :big))
+(s/def :big/int32 (e/primitive Integer :order :big))
+(s/def :big/int64 (e/primitive Long :order :big))
+(s/def :big/uint8 (e/unsigned-primitive Byte :order :big))
+(s/def :big/uint16 (e/unsigned-primitive Short :order :big))
+(s/def :big/uint32 (e/unsigned-primitive Integer :order :big))
+(s/def :big/float (e/floating-primitive Float :order :big))
+(s/def :big/double (e/floating-primitive Double :order :big))
 
-(s/def :aligned/int8 (e/primitive Byte ::e/word-size 4))
-(s/def :aligned/int16 (e/primitive Short ::e/word-size 4))
-(s/def :aligned/int32 (e/primitive Integer ::e/word-size 4))
-(s/def :aligned/int64 (e/primitive Long ::e/word-size 4))
-(s/def :aligned/uint8 (e/unsigned-primitive Byte ::e/word-size 4))
-(s/def :aligned/uint16 (e/unsigned-primitive Short ::e/word-size 4))
-(s/def :aligned/uint32 (e/unsigned-primitive Integer ::e/word-size 4))
-(s/def :aligned/float (e/floating-primitive Float ::e/word-size 4))
-(s/def :aligned/double (e/floating-primitive Double ::e/word-size 4))
+(s/def :aligned/int8 (e/primitive Byte :word-size 4))
+(s/def :aligned/int16 (e/primitive Short :word-size 4))
+(s/def :aligned/int32 (e/primitive Integer :word-size 4))
+(s/def :aligned/int64 (e/primitive Long :word-size 4))
+(s/def :aligned/uint8 (e/unsigned-primitive Byte :word-size 4))
+(s/def :aligned/uint16 (e/unsigned-primitive Short :word-size 4))
+(s/def :aligned/uint32 (e/unsigned-primitive Integer :word-size 4))
+(s/def :aligned/float (e/floating-primitive Float :word-size 4))
+(s/def :aligned/double (e/floating-primitive Double :word-size 4))
 
-(s/def :aligned-wide/int8 (e/primitive Byte ::e/word-size 8))
-(s/def :aligned-wide/int16 (e/primitive Short ::e/word-size 8))
-(s/def :aligned-wide/int32 (e/primitive Integer ::e/word-size 8))
-(s/def :aligned-wide/int64 (e/primitive Long ::e/word-size 8))
-(s/def :aligned-wide/uint8 (e/unsigned-primitive Byte ::e/word-size 8))
-(s/def :aligned-wide/uint16 (e/unsigned-primitive Short ::e/word-size 8))
-(s/def :aligned-wide/uint32 (e/unsigned-primitive Integer ::e/word-size 8))
-(s/def :aligned-wide/float (e/floating-primitive Float ::e/word-size 8))
-(s/def :aligned-wide/double (e/floating-primitive Double ::e/word-size 8))
+(s/def :aligned-wide/int8 (e/primitive Byte :word-size 8))
+(s/def :aligned-wide/int16 (e/primitive Short :word-size 8))
+(s/def :aligned-wide/int32 (e/primitive Integer :word-size 8))
+(s/def :aligned-wide/int64 (e/primitive Long :word-size 8))
+(s/def :aligned-wide/uint8 (e/unsigned-primitive Byte :word-size 8))
+(s/def :aligned-wide/uint16 (e/unsigned-primitive Short :word-size 8))
+(s/def :aligned-wide/uint32 (e/unsigned-primitive Integer :word-size 8))
+(s/def :aligned-wide/float (e/floating-primitive Float :word-size 8))
+(s/def :aligned-wide/double (e/floating-primitive Double :word-size 8))
 
 
 
@@ -109,5 +109,46 @@
       (is (s/invalid? (s/conform :little/int16 1.02)))
       (is (s/invalid? (s/conform :little/int32 1.02)))
       (is (s/invalid? (s/conform :little/int64 1.02))))))
+
+
+(testing "primitive codec binary"
+  (let [byte-test {:value 0x17 :little (map unchecked-byte [0x17]) :big (map unchecked-byte [0x17])} 
+        short-test {:value 0x1234 :little (map unchecked-byte [0x34 0x12]) :big (map unchecked-byte [0x12 0x34])}
+        int-test {:value (unchecked-int 0xDEADBEEF)
+                  :little (map unchecked-byte [0xEF 0xBE 0xAD 0xDE])
+                  :big (map unchecked-byte [0xDE 0xAD 0xBE 0xEF])} 
+        long-test {:value 0x12345678DEADBEEF 
+                   :little (map unchecked-byte [0xEF 0xBE 0xAD 0xDE 0x78 0x56 0x34 0x12]) 
+                   :big (map unchecked-byte [0x12 0x34 0x56 0x78 0xDE 0xAD 0xBE 0xEF])}]
+    (testing "encoding"
+      (testing "nil value encoding"
+        (is (= (repeat 1 0) (e/encode :little/int8 nil)))
+        (is (= (repeat 2 0) (e/encode :little/int16 nil)))
+        (is (= (repeat 4 0) (e/encode :little/int32 nil)))
+        (is (= (repeat 8 0) (e/encode :little/int64 nil))))
+      (testing "little endian"
+        (is (= (:little byte-test) (e/encode :little/int8 (:value byte-test))))
+        (is (= (:little short-test) (e/encode :little/int16 (:value short-test))))
+        (is (= (:little int-test) (e/encode :little/int32 (:value int-test))))
+        (is (= (:little long-test) (e/encode :little/int64 (:value long-test)))))
+      (testing "big endian"
+        (is (= (:big byte-test) (e/encode :big/int8 (:value byte-test))))
+        (is (= (:big short-test) (e/encode :big/int16 (:value short-test))))
+        (is (= (:big int-test) (e/encode :big/int32 (:value int-test))))
+        (is (= (:big long-test) (e/encode :big/int64 (:value long-test))))))
+    (testing "decoding"
+      (testing "little endian"
+        (is (= (:value byte-test) (first (e/decode :little/int8 (:little byte-test)))))
+        (is (= (:value short-test) (first (e/decode :little/int16 (:little short-test)))))
+        (is (= (:value int-test) (first (e/decode :little/int32 (:little int-test)))))
+        (is (= (:value long-test) (first (e/decode :little/int64 (:little long-test))))))
+      (testing "big endian"
+        (is (= (:value byte-test) (first (e/decode :big/int8 (:big byte-test))))
+        (is (= (:value short-test) (first (e/decode :big/int16 (:big short-test))))
+        (is (= (:value int-test) (first (e/decode :big/int32 (:big int-test))))
+        (is (= (:value long-test) (first (e/decode :big/int64 (:big long-test))))))))))
+    (testing "unsigned decoding"
+      (is (= 0xFE (first (e/decode :little/uint8 [(unchecked-byte 0xFE)]))))
+      (is (= 0xDEADBEEF (first (e/decode :little/uint32 (:little int-test))))))))
 
 
