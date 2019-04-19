197aa08b0807a6f868cb532c2bf2453bbb2e2cc2
Richard Hanley <richard@rhanley.com>
2019-04-19 16:53:12 -0500

    Adding in tests for primitive specs

--- src/encode_binary/core.clj
+++ src/encode_binary/core.clj
@@ -34,21 +34,26 @@
 (defn- specize [x]
     (if (keyword? x) (reg-resolve! x) x))
 
+(def default-order ByteOrder/LITTLE_ENDIAN)
+(def order-map {:little ByteOrder/LITTLE_ENDIAN
+                :big ByteOrder/BIG_ENDIAN
+                :native (ByteOrder/nativeOrder)
+                :network ByteOrder/BIG_ENDIAN})
 (defprotocol Binary
   :extend-via-metadata true
   (alignment* [this])
-  (sizeof* [this])
+  (sizeof* [this]))
 
 (defprotocol Codec
   :extend-via-metadata true
   (encode* [this data])
   (decode* [this binary-seq]))
 
-(defn codec? [maybe-c])
-
 (defn codify [x enc dec & {:keys [alignment size]
                            :or {alignment nil size nil}}]
-  (with-meta (s/spec x) 
+  (with-meta (if (s/spec? x)
+               x
+               (s/spec x))
              (assoc (meta x)
                     `encode* enc
                     `decode* dec
@@ -68,6 +73,94 @@
 (defn decode [codec binary-seq]
   (decode* (specize codec) binary-seq))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Defining Primitive Codecs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn -main []
-  (println "Hello World"))
+(def primitive-ops
+  {Byte {:get-buffer #(.get %) :put-buffer #(.put %1 (unchecked-byte %2))}
+   Short {:get-buffer #(.getShort %) :put-buffer #(.putShort %1 (unchecked-short %2))}
+   Integer {:get-buffer #(.getInt %) :put-buffer #(.putInt %1 (unchecked-int %2))}
+   Long {:get-buffer #(.getLong %) :put-buffer #(.putLong %1 (unchecked-long %2))}
+   Float {:get-buffer #(.getFloat %) :put-buffer #(.putFloat %1 (unchecked-float %2))}
+   Double {:get-buffer #(.getDouble %) :put-buffer #(.putDouble %1 (unchecked-double %2))}})
+
+
+
+(defn signed-primitive-spec [min-value max-value]
+  `(s/and int? 
+          #(<= ~min-value % ~max-value)))
+
+(defn floating-primitive-spec [min-value max-value]
+  `#(or (zero? %)
+        (<= ~min-value % ~max-value)))
+
+; (defmacro unsigned-primitive-spec [class]
+
+; (defn prim-size [conv] `(. ~(type (conv 0)) ~'SIZE))
+
+; (defmacro put-prim [put conv order]
+;   `(fn [buffer value] (. buffer (~conv value)gt
+
+(defn put-prim [put-buffer size order]
+  (fn [_ data]
+    (let [buff (.order (ByteBuffer/allocate size)
+                       (get order-map order default-order))
+          _ (put-buffer buff (or data 0))]
+      (seq (.array buff)))))
+
+(defn get-prim [get-buffer size order]
+  (fn [_ binary-seq]
+    (let [[prim remaining] (split-at size binary-seq)
+          bytes (.order (ByteBuffer/wrap (byte-array prim))
+                        (get order-map order default-order))]
+      [(get-buffer bytes) remaining])))
+
+(defmacro primitive [prim & {:keys [word-size order]
+                             :or {word-size 1}}]
+  (let [size `(. ~prim ~'BYTES)
+        min-value `(. ~prim ~'MIN_VALUE)
+        max-value `(. ~prim ~'MAX_VALUE)
+        ops `(get primitive-ops ~prim)]
+    `(codify (s/spec* (signed-primitive-spec ~min-value ~max-value))
+             (put-prim (:put-buffer ~ops) ~size ~order)
+             (get-prim (:get-buffer ~ops) ~size ~order)
+             :alignment (min ~size ~word-size)
+             :size ~size)))
+
+(defmacro unsigned-primitive [prim & {:keys [word-size order]
+                             :or {word-size 1}}]
+  (let [size `(. ~prim ~'BYTES)
+        max-value `(bit-shift-left 1 (. ~prim ~'SIZE))
+        to-unsigned `#(. ~prim toUnsignedLong %)
+        ops `(get primitive-ops ~prim)]
+    `(codify (s/int-in 0 ~max-value)
+             (put-prim (:put-buffer ~ops) ~size ~order)
+             (get-prim (comp ~to-unsigned (:get-buffer ~ops)) ~size ~order)
+             :alignment (min ~size ~word-size)
+             :size ~size)))
+
+(defmacro floating-primitive [prim & {:keys [word-size order]
+                             :or {word-size 1}}]
+  (let [size `(. ~prim ~'BYTES)
+        min-value `(. ~prim ~'MIN_VALUE)
+        max-value `(. ~prim ~'MAX_VALUE)
+        ops `(get primitive-ops ~prim)]
+    `(codify (s/spec* (floating-primitive-spec ~min-value ~max-value))
+             (put-prim (:put-buffer ~ops) ~size ~order)
+             (get-prim (:get-buffer ~ops) ~size ~order)
+             :alignment (min ~size ~word-size)
+             :size ~size)))
+
+(s/def ::int8 (primitive Byte))
+(s/def ::int16 (primitive Short))
+(s/def ::int32 (primitive Integer))
+(s/def ::int64 (primitive Long))
+
+(s/def ::uint8 (unsigned-primitive Byte))
+(s/def ::uint16 (unsigned-primitive Short))
+(s/def ::uint32 (unsigned-primitive Integer))
+
+(s/def ::float (floating-primitive Float))
+(s/def ::double (floating-primitive Double))
+
--- test/encode_binary/core_test.clj
+++ test/encode_binary/core_test.clj
@@ -1,8 +1,113 @@
 (ns encode-binary.core-test
     (:require [clojure.test :refer :all]
-              [clojure.spec.alpha :as s]
+              [clojure.spec-alpha2 :as s]
               [encode-binary.core :as e]))
 
-(deftest addition
-  (is (= 4 (+ 2 2)))
-  (is (= 5 (+ 2 2))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Redefining primitives
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(s/def :little/int8 (e/primitive Byte))
+(s/def :little/int16 (e/primitive Short))
+(s/def :little/int32 (e/primitive Integer))
+(s/def :little/int64 (e/primitive Long))
+(s/def :little/uint8 (e/unsigned-primitive Byte))
+(s/def :little/uint16 (e/unsigned-primitive Short))
+(s/def :little/uint32 (e/unsigned-primitive Integer))
+(s/def :little/float (e/floating-primitive Float))
+(s/def :little/double (e/floating-primitive Double))
+
+(s/def :big/int8 (e/primitive Byte ::e/order :big))
+(s/def :big/int16 (e/primitive Short ::e/order :big))
+(s/def :big/int32 (e/primitive Integer ::e/order :big))
+(s/def :big/int64 (e/primitive Long ::e/order :big))
+(s/def :big/uint8 (e/unsigned-primitive Byte ::e/order :big))
+(s/def :big/uint16 (e/unsigned-primitive Short ::e/order :big))
+(s/def :big/uint32 (e/unsigned-primitive Integer ::e/order :big))
+(s/def :big/float (e/floating-primitive Float ::e/order :big))
+(s/def :big/double (e/floating-primitive Double ::e/order :big))
+
+(s/def :aligned/int8 (e/primitive Byte ::e/word-size 4))
+(s/def :aligned/int16 (e/primitive Short ::e/word-size 4))
+(s/def :aligned/int32 (e/primitive Integer ::e/word-size 4))
+(s/def :aligned/int64 (e/primitive Long ::e/word-size 4))
+(s/def :aligned/uint8 (e/unsigned-primitive Byte ::e/word-size 4))
+(s/def :aligned/uint16 (e/unsigned-primitive Short ::e/word-size 4))
+(s/def :aligned/uint32 (e/unsigned-primitive Integer ::e/word-size 4))
+(s/def :aligned/float (e/floating-primitive Float ::e/word-size 4))
+(s/def :aligned/double (e/floating-primitive Double ::e/word-size 4))
+
+(s/def :aligned-wide/int8 (e/primitive Byte ::e/word-size 8))
+(s/def :aligned-wide/int16 (e/primitive Short ::e/word-size 8))
+(s/def :aligned-wide/int32 (e/primitive Integer ::e/word-size 8))
+(s/def :aligned-wide/int64 (e/primitive Long ::e/word-size 8))
+(s/def :aligned-wide/uint8 (e/unsigned-primitive Byte ::e/word-size 8))
+(s/def :aligned-wide/uint16 (e/unsigned-primitive Short ::e/word-size 8))
+(s/def :aligned-wide/uint32 (e/unsigned-primitive Integer ::e/word-size 8))
+(s/def :aligned-wide/float (e/floating-primitive Float ::e/word-size 8))
+(s/def :aligned-wide/double (e/floating-primitive Double ::e/word-size 8))
+
+
+
+(testing "primitive specs"
+  (testing "signed"
+    (testing "zero checking"
+      (is (= 0 (s/conform :little/int8 0)))
+      (is (= 0 (s/conform :little/int16 0)))
+      (is (= 0 (s/conform :little/int32 0)))
+      (is (= 0 (s/conform :little/int64 0)))
+      (is (= 0.0 (s/conform :little/float 0.0)))
+      (is (= 0.0 (s/conform :little/double 0.0))))
+    (testing "max value checking"
+      (testing "at max value"
+        (is (= Byte/MAX_VALUE (s/conform :little/int8 Byte/MAX_VALUE)))
+        (is (= Short/MAX_VALUE (s/conform :little/int16 Short/MAX_VALUE)))
+        (is (= Integer/MAX_VALUE (s/conform :little/int32 Integer/MAX_VALUE)))
+        (is (= Long/MAX_VALUE (s/conform :little/int64 Long/MAX_VALUE)))
+        (is (= Float/MAX_VALUE (s/conform :little/float Float/MAX_VALUE)))
+        (is (= Double/MAX_VALUE (s/conform :little/double Double/MAX_VALUE))))
+      (testing "beyond max value"
+        (is (s/invalid? (s/conform :little/int8 (inc Byte/MAX_VALUE))))
+        (is (s/invalid? (s/conform :little/int16 (inc Short/MAX_VALUE))))
+        (is (s/invalid? (s/conform :little/int32 (inc Integer/MAX_VALUE))))))
+    (testing "min value checking"
+      (testing "at max value"
+        (is (= Byte/MIN_VALUE (s/conform :little/int8 Byte/MIN_VALUE)))
+        (is (= Short/MIN_VALUE (s/conform :little/int16 Short/MIN_VALUE)))
+        (is (= Integer/MIN_VALUE (s/conform :little/int32 Integer/MIN_VALUE)))
+        (is (= Long/MIN_VALUE (s/conform :little/int64 Long/MIN_VALUE)))
+        (is (= Float/MIN_VALUE (s/conform :little/float Float/MIN_VALUE)))
+        (is (= Double/MIN_VALUE (s/conform :little/double Double/MIN_VALUE))))
+      (testing "beyond max value"
+        (is (s/invalid? (s/conform :little/int8 (dec Byte/MIN_VALUE))))
+        (is (s/invalid? (s/conform :little/int16 (dec Short/MIN_VALUE))))
+        (is (s/invalid? (s/conform :little/int32 (dec Integer/MIN_VALUE))))))
+    (testing "type checking"
+      (is (s/invalid? (s/conform :little/int8 1.02)))
+      (is (s/invalid? (s/conform :little/int16 1.02)))
+      (is (s/invalid? (s/conform :little/int32 1.02)))
+      (is (s/invalid? (s/conform :little/int64 1.02)))))
+  (testing "unsigned"
+    (testing "zero checking"
+      (is (= 0 (s/conform :little/uint8 0)))
+      (is (= 0 (s/conform :little/uint16 0)))
+      (is (= 0 (s/conform :little/uint32 0))))
+    (testing "max value checking"
+      (testing "at max value"
+        (is (= 0xff (s/conform :little/uint8 0xff)))
+        (is (= 0xffff (s/conform :little/uint16 0xffff)))
+        (is (= 0xffffffff (s/conform :little/uint32 0xffffffff))))
+      (testing "beyond max value"
+        (is (s/invalid? (s/conform :little/uint8 0x100)))
+        (is (s/invalid? (s/conform :little/uint16 0x10000)))
+        (is (s/invalid? (s/conform :little/uint32 0x100000000)))))
+    (testing "negative values"
+      (is (s/invalid? (s/conform :little/uint8 -1)))
+      (is (s/invalid? (s/conform :little/uint16 -1)))
+      (is (s/invalid? (s/conform :little/uint32 -1))))
+    (testing "type checking"
+      (is (s/invalid? (s/conform :little/int8 1.02)))
+      (is (s/invalid? (s/conform :little/int16 1.02)))
+      (is (s/invalid? (s/conform :little/int32 1.02)))
+      (is (s/invalid? (s/conform :little/int64 1.02))))))
+
+
