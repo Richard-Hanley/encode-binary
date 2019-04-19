0da38b2d50d034a8579c8c37b326c0d896b54eb9
Richard Hanley <richard@rhanley.com>
2019-04-19 13:55:34 -0500

    Adding in scaffolding around spec

--- src/encode_binary/core.clj
+++ src/encode_binary/core.clj
@@ -6,20 +6,68 @@
   (:require [clojure.spec-alpha2 :as s]
             [clojure.set :as set]))
 
+(defn- deep-resolve [reg k]
+  (loop [spec k]
+    (if (ident? spec)
+      (recur (get reg spec))
+      spec)))
+
+(defn- reg-resolve
+  "returns the spec/regex at end of alias chain starting with k, nil if not found, k if k not ident"
+  [k]
+  (if (ident? k)
+    (let [reg (s/registry)
+          spec (get reg k)]
+      (if-not (ident? spec)
+        spec
+        (deep-resolve reg spec)))
+    k))
+
+(defn- reg-resolve!
+  "returns the spec/regex at end of alias chain starting with k, throws if not found, k if k not ident"
+  [k]
+  (if (ident? k)
+    (or (reg-resolve k)
+          (throw (Exception. (str "Unable to resolve spec: " k))))
+    k))
+
+(defn- specize [x]
+    (if (keyword? x) (reg-resolve! x) x))
+
 (defprotocol Binary
   :extend-via-metadata true
-  (alignment* [this] "The alignment of this binary.  The resulting value will conform to :encode-binary/alignment")
-  (sizeof* [this] "The size in bytes of the binary.  If the size cannot be determined with the current value, then it will return nil"))
+  (alignment* [this])
+  (sizeof* [this])
 
 (defprotocol Codec
   :extend-via-metadata true
   (encode* [this data])
   (decode* [this binary-seq]))
 
-(defn codify [])
+(defn codec? [maybe-c])
+
+(defn codify [x enc dec & {:keys [alignment size]
+                           :or {alignment nil size nil}}]
+  (with-meta (s/spec x) 
+             (assoc (meta x)
+                    `encode* enc
+                    `decode* dec
+                    `alignment* (constantly alignment)
+                    `sizeof* (constantly size))))
 (defn specify [])
 
-(defn foo [a b] (+ a b))
+(defn alignment [bin]
+  (alignment* (specize bin)))
+
+(defn sizeof [bin]
+  (sizeof* (specize bin)))
+
+(defn encode [codec data]
+  (encode* (specize codec) data))
+
+(defn decode [codec binary-seq]
+  (decode* (specize codec) binary-seq))
+
 
 (defn -main []
   (println "Hello World"))
