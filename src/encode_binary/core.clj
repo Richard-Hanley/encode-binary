0377068efe2721ecc309c8ea423e3f787d7d52fd
Richard Hanley <richard@rhanley.com>
2019-04-19 13:11:50 -0500

    Adding in protocol definitions

--- src/encode_binary/core.clj
+++ src/encode_binary/core.clj
@@ -6,6 +6,19 @@
   (:require [clojure.spec-alpha2 :as s]
             [clojure.set :as set]))
 
+(defprotocol Binary
+  :extend-via-metadata true
+  (alignment* [this] "The alignment of this binary.  The resulting value will conform to :encode-binary/alignment")
+  (sizeof* [this] "The size in bytes of the binary.  If the size cannot be determined with the current value, then it will return nil"))
+
+(defprotocol Codec
+  :extend-via-metadata true
+  (encode* [this data])
+  (decode* [this binary-seq]))
+
+(defn codify [])
+(defn specify [])
+
 (defn foo [a b] (+ a b))
 
 (defn -main []
